b495096f3398a2390712fe1a5f761ecce56fe417
siva <siva636@gmail.com>
2025-02-14 13:28:11 +0530

    Brand logo added

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,5 +1,5 @@
 import CartModal from 'components/cart/modal';
-import LogoSquare from 'components/logo-square';
+import Logo from 'components/logo';
 import { getMenu } from 'lib/shopify';
 import { Menu } from 'lib/shopify/types';
 import Link from 'next/link';
@@ -13,32 +13,32 @@ export async function Navbar() {
   const menu = await getMenu('next-js-frontend-header-menu');
 
   return (
-    <nav className="relative flex items-center justify-between p-4 lg:px-6">
-      <div className="block flex-none md:hidden">
+    <nav className='relative flex items-center justify-between p-4 lg:px-6'>
+      <div className='block flex-none md:hidden'>
         <Suspense fallback={null}>
           <MobileMenu menu={menu} />
         </Suspense>
       </div>
-      <div className="flex w-full items-center">
-        <div className="flex w-full md:w-1/3">
+      <div className='flex w-full items-center'>
+        <div className='flex w-full md:w-1/3'>
           <Link
-            href="/"
+            href='/'
             prefetch={true}
-            className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
+            className='mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6'
           >
-            <LogoSquare />
-            <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
+            <Logo />
+            {/* <div className='ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block'>
               {SITE_NAME}
-            </div>
+            </div> */}
           </Link>
           {menu.length ? (
-            <ul className="hidden gap-6 text-sm md:flex md:items-center">
+            <ul className='hidden gap-6 text-sm md:flex md:items-center'>
               {menu.map((item: Menu) => (
                 <li key={item.title}>
                   <Link
                     href={item.path}
                     prefetch={true}
-                    className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
+                    className='text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300'
                   >
                     {item.title}
                   </Link>
@@ -47,12 +47,12 @@ export async function Navbar() {
             </ul>
           ) : null}
         </div>
-        <div className="hidden justify-center md:flex md:w-1/3">
+        <div className='hidden justify-center md:flex md:w-1/3'>
           <Suspense fallback={<SearchSkeleton />}>
             <Search />
           </Suspense>
         </div>
-        <div className="flex justify-end md:w-1/3">
+        <div className='flex justify-end md:w-1/3'>
           <CartModal />
         </div>
       </div>
---
+++ components/logo.tsx
@@ -0,0 +1,5 @@
+import Image from 'next/image';
+
+export default function Logo() {
+  return <Image src='/logo.png' width={150} height={63} alt='Logo' />;
+}
